31dba66b1fb802dd16dcc7a48dfadf80cd92eddb
Dalton <murraydalton1@comcast.net>
2025-02-23 14:13:41 -0500

    Update constants.py

--- megalinter/constants.py
+++ megalinter/constants.py
@@ -1,6 +1,6 @@
 from megalinter import config
 
-ML_REPO_OWNER = "oxsecurity"
+ML_REPO_OWNER = "Dalton-Murray"
 ML_REPO_NAME = "megalinter"
 ML_REPO = f"{ML_REPO_OWNER}/{ML_REPO_NAME}"
 ML_REPO_URL = f"https://github.com/{ML_REPO_OWNER}/{ML_REPO_NAME}"
@@ -10,7 +10,7 @@ ML_DOC_URL = ML_DOC_URL_BASE + (ML_VERSION if len(ML_VERSION) > 1 else "latest")
 ML_REPO_ISSUES_URL = f"https://github.com/{ML_REPO_OWNER}/{ML_REPO_NAME}/issues"
 ML_DOC_URL_DESCRIPTORS_ROOT = f"{ML_DOC_URL}/descriptors"
 
-ML_DOCKER_OWNER = "oxsecurity"
+ML_DOCKER_OWNER = "Dalton-Murray"
 ML_DOCKER_NAME = "megalinter"
 ML_DOCKER_IMAGE = f"{ML_DOCKER_OWNER}/{ML_DOCKER_NAME}"
 ML_DOCKER_IMAGE_LEGACY = "nvuillam/mega-linter"
